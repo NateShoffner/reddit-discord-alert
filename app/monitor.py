4728b7ef92da8f6c6770d4a731ff13dae53976e7
Nate Shoffner <nate.shoffner@gmail.com>
2023-11-25 12:10:31 -0500

    Scuffed caching

--- app/monitor.py
+++ app/monitor.py
@@ -2,7 +2,9 @@ import cachetools
 import asyncpraw
 
 import asyncio
+import json
 import logging
+import os
 
 from discord_handler import DiscordHandler
 
@@ -16,7 +18,27 @@ class SubredditMonitor:
         self.discord_handler = discord_handler
         self.logger = logging.getLogger(self.__class__.__name__)
         self.post_cache = cachetools.TTLCache(maxsize=256, ttl=604800)
-        # TODO persist cache to disk so we don't have to re-share posts on restart
+
+        self.load_cache()
+
+    def load_cache(self):
+        try:
+            with open(os.getenv("CACHE_NAME"), "r") as f:
+                for k, v in json.load(f).items():
+                    self.post_cache[k] = v
+
+            self.logger.info(f"Loaded {len(self.post_cache)} items from cache.")
+        except json.decoder.JSONDecodeError:
+            self.logger.warning("Cache file is corrupted, starting fresh.")
+        except FileNotFoundError:
+            self.logger.warning("Cache file not found, starting fresh.")
+
+    def save_cache(self):
+        temp_dict = {}
+        for k, v in self.post_cache.items():
+            temp_dict[k] = v
+        with open(os.getenv("CACHE_NAME"), "w") as f:
+            json.dump(temp_dict, f)
 
     async def poll(self, interval):
         while True:
@@ -37,4 +59,5 @@ class SubredditMonitor:
             self.logger.info(f"Found new post: {submission.title}")
 
             share_result = await self.discord_handler.share_post(submission)
-            self.post_cache[submission.id] = share_result
+            self.post_cache[submission.id] = share_result.status_code
+            self.save_cache()
